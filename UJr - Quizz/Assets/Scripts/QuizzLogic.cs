ba50504a29435c1f0a7582341431cfc9f93c5080
x0112358 <svmirjf@gmail.com>
2017-07-05 16:42:03 +0100

    Final Version of QuizzScript

--- UJr - Quizz/Assets/Scripts/QuizzLogic.cs
+++ UJr - Quizz/Assets/Scripts/QuizzLogic.cs
@@ -7,33 +7,73 @@ public class QuizzLogic : MonoBehaviour
 
     public int nPlayers; //Number of players variable
 
-    public LinkedList<Question> QuestionList; //start by having a list of objects (question objects)
+    public LinkedList<Question> QuestionList = new LinkedList<Question>(); //start by having a list of objects (question objects)
 
     public string[] QuestionsArray;
-    public string[] Answer;
-    public string[] Questions;
+    public string[] CorrectAnswer;
+    public string[] PossibleAnswers;
+
+    UnityEngine.UI.Button button1;
+    UnityEngine.UI.Button button2;
+    UnityEngine.UI.Button button3;
+    UnityEngine.UI.Button button4;
+
+    Question currentQuestion;
 
     // Use this for initialization
     void Start()
     {
 
+        button1 = GameObject.Find("B1").GetComponent<UnityEngine.UI.Button>();
+        button2 = GameObject.Find("B2").GetComponent<UnityEngine.UI.Button>();
+        button3 = GameObject.Find("B3").GetComponent<UnityEngine.UI.Button>();
+        button4 = GameObject.Find("B4").GetComponent<UnityEngine.UI.Button>();
+
+        button1.onClick.AddListener(() => choseAnswer(1));
+        button2.onClick.AddListener(() => choseAnswer(2));
+        button3.onClick.AddListener(() => choseAnswer(3));
+        button4.onClick.AddListener(() => choseAnswer(4));
+
         for (int i = 0; i < QuestionsArray.Length; i++)
         {
-            Question cQuestion = new Question();
+            string[] pAnswers = new string[4];
+            for(int j = 0; j < 4; j++)
+            {
+                pAnswers[j] = PossibleAnswers[i + j]; 
+            }
+
             //construct the question
+            Question cQuestion = new Question(QuestionsArray[i],CorrectAnswer[i],pAnswers);
+
+            Debug.Log(cQuestion.question + " - " + cQuestion.correctAnsewer);
+
             //add the element to the list
             QuestionList.AddLast(cQuestion);
         }
 
+        nextQuestion();
+
+    }
+
+    void choseAnswer(int i)
+    {
+        Debug.Log(i);
     }
 
     void nextQuestion()
     {
         //pick a question randomly
         int n = Random.Range(0, QuestionList.Count);
-        Question currentQuestion = getElement(QuestionList, n);
+        Debug.Log("random:"+n);
+        currentQuestion = getElement(QuestionList, n);
 
-        Debug.logger.Log(currentQuestion.question);
+        GameObject.Find("QuestionText").GetComponent<UnityEngine.UI.Text>().text = currentQuestion.question;
+
+        GameObject.Find("A1").GetComponent<UnityEngine.UI.Text>().text = currentQuestion.PossibleAnswers[0];
+        GameObject.Find("A2").GetComponent<UnityEngine.UI.Text>().text = currentQuestion.PossibleAnswers[1];
+        GameObject.Find("A3").GetComponent<UnityEngine.UI.Text>().text = currentQuestion.PossibleAnswers[2];
+        GameObject.Find("A4").GetComponent<UnityEngine.UI.Text>().text = currentQuestion.PossibleAnswers[3];
+
     }
 
     // Update is called once per frame
@@ -46,10 +86,12 @@ public class QuizzLogic : MonoBehaviour
     {
         LinkedListNode<Question> cursor = start.First;
         int counter = 0;
-        while (counter <= index)
+        while (counter < index)
         {
             cursor = cursor.Next;
+            counter++;
         }
+        Debug.Log(counter);
         return cursor.Value;
     }
 }
@@ -58,12 +100,17 @@ public class Question
 {
     //each question will have a list of possible answers, the correct answer and the question
     public string question;
-    public LinkedList<string> PossibleAnswers = new LinkedList<string>();
+    public string[] PossibleAnswers = new string[4];
     public string correctAnsewer;
 
-    public Question() //Constructor for the Question data structure
+    public Question(string q, string cAnswer, string[] pAnswers) //Constructor for the Question data structure
     {
-
+        this.question = q;
+        this.correctAnsewer = cAnswer;
+        for(int i = 0; i < pAnswers.Length; i++)
+        {
+            this.PossibleAnswers[i] = pAnswers[i];
+        }
     }
 
 }
