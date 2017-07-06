cb0fb7dd018566b25e2f8b78de2986b1ba5b9ff3
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-07-06 18:08:59 +0100

    PlayerSelection

---
+++ UJr - Quizz/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public GameObject playerPos;
+    public List<GameObject> PlayerList;
+
+    private int index;
+
+    public void Start()
+    {
+        index = 0;
+        Player.currentPlayer = Instantiate(PlayerList[index], playerPos.transform);
+       
+    }
+    public void LeftArrow()
+    {
+        if(index > 0)
+        {
+            Destroy(Player.currentPlayer);
+            index--;
+            Player.currentPlayer = Instantiate(PlayerList[index], playerPos.transform);
+        }
+    }
+    public void RighArrow()
+    {
+        if(index < PlayerList.Count - 1)
+        {
+            Destroy(Player.currentPlayer);
+            index++;
+            Player.currentPlayer = Instantiate(PlayerList[index], playerPos.transform);
+        }
+    }
+}
+public class Player
+{
+    public static GameObject currentPlayer;
+}
