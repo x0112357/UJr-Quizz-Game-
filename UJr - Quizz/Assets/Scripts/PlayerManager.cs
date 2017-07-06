902ca75b82a1e8786bb7ed6fe201fc8302b8e6ef
x0112358 <svmirjf@gmail.com>
2017-07-06 18:24:25 +0100

    merge

--- UJr - Quizz/Assets/Scripts/PlayerManager.cs
+++ UJr - Quizz/Assets/Scripts/PlayerManager.cs
@@ -6,35 +6,36 @@ public class PlayerManager : MonoBehaviour {
 
     public GameObject playerPos;
     public List<GameObject> PlayerList;
+    public static GameObject currentPlayer;
+
+    private GameObject aux;
 
     private int index;
 
     public void Start()
     {
         index = 0;
-        Player.currentPlayer = Instantiate(PlayerList[index], playerPos.transform);
-       
+        aux = Instantiate(PlayerList[index], playerPos.transform);
+        currentPlayer = PlayerList[index];
     }
     public void LeftArrow()
     {
         if(index > 0)
         {
-            Destroy(Player.currentPlayer);
+            Destroy(aux);
             index--;
-            Player.currentPlayer = Instantiate(PlayerList[index], playerPos.transform);
+            aux = Instantiate(PlayerList[index], playerPos.transform);
+            currentPlayer = PlayerList[index];
         }
     }
     public void RighArrow()
     {
         if(index < PlayerList.Count - 1)
         {
-            Destroy(Player.currentPlayer);
+            Destroy(aux);
             index++;
-            Player.currentPlayer = Instantiate(PlayerList[index], playerPos.transform);
+            aux = Instantiate(PlayerList[index], playerPos.transform);
+            currentPlayer = PlayerList[index];
         }
     }
-}
-public class Player
-{
-    public static GameObject currentPlayer;
 }
