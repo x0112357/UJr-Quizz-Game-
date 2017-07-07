e60d003576fe1da54359edb00b21bc6ef803e8d4
x0112358 <svmirjf@gmail.com>
2017-07-08 00:33:23 +0100

    Save the instatiate of the player in cPlayer

--- UJr - Quizz/Assets/Scripts/SpawnPlayer.cs
+++ UJr - Quizz/Assets/Scripts/SpawnPlayer.cs
@@ -6,11 +6,11 @@ public class SpawnPlayer : MonoBehaviour
 {
     public GameObject scenePos;
     public GameObject playerPos;
-
+    public static GameObject cPlayer;
     void Start()
     {
         Instantiate(PlayerManager.currentScene, scenePos.transform);
         if (PlayerManager.currentPlayer != null)
-            Instantiate(PlayerManager.currentPlayer, playerPos.transform);
+            cPlayer = Instantiate(PlayerManager.currentPlayer, playerPos.transform);
     }
 }
