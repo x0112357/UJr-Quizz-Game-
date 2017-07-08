b87e7f79ea645e0ea0a97035de2a7e2e2a90c553
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-07-08 19:41:45 +0100

    Animations

--- UJr - Quizz/Assets/Scripts/SpawnPlayer.cs
+++ UJr - Quizz/Assets/Scripts/SpawnPlayer.cs
@@ -6,11 +6,12 @@ public class SpawnPlayer : MonoBehaviour
 {
     public GameObject scenePos;
     public GameObject playerPos;
+    public bool spawnAll = false;
     public static GameObject cPlayer;
     void Start()
     {
         Instantiate(PlayerManager.currentScene, scenePos.transform);
-        if (PlayerManager.currentPlayer != null)
+        if (spawnAll)
             cPlayer = Instantiate(PlayerManager.currentPlayer, playerPos.transform);
     }
 }
