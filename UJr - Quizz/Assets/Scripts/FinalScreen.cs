b9ebeeacd74b1ee01abc90411c7cdbcab3d9d4d0
x0112358 <svmirjf@gmail.com>
2017-07-09 02:07:44 +0100

    Final Screen with score and try again button

---
+++ UJr - Quizz/Assets/Scripts/FinalScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FinalScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject.Find("Score").GetComponent<Text>().text = "Your Score: "+QuizzLogic.score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void tryAgain()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
